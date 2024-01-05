83d2799cee183e5e0b59d53ef92e5f6dfba9356d
realskyquest <135049479+realskyquest@users.noreply.github.com>
2024-01-05 23:11:39 +0500

    Added client

---
+++ client--tracking-pixel/src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<svelte:head>
+	<title>Tracking pixel - realskyquest - Home</title>
+</svelte:head>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p>All hands on deck.</p>
